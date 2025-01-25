b3170fc4b49806a3151bea66a077a97167d702c2
zqy1018 <zqy1018@sjtu.edu.cn>
2025-01-25 20:25:43 +0800

    bump to Lean v4.15.0

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,8 @@
 import Lake
 open Lake DSL
 
-require aesop from git "https://github.com/leanprover-community/aesop" @ "v4.15.0"
+require aesop from git "https://github.com/leanprover-community/aesop.git" @ "v4.15.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "v4.15.0-patch1"
 
 package «lentil» where
   -- Settings applied to both builds and interactive editing
@@ -14,3 +15,6 @@ package «lentil» where
 @[default_target]
 lean_lib «Lentil» where
   -- add any library configuration options here
+
+lean_lib Examples where
+  globs := #[.submodules `Examples]
