6cd95fbe38304458bd53ba350636d1f959bc51bd
zqy1018 <zqy1018@sjtu.edu.cn>
2025-02-18 23:49:26 +0800

    comment on the `simp_attr`s

--- Lentil/Util.lean
+++ Lentil/Util.lean
@@ -55,9 +55,16 @@ register_option lentil.pp.autoRenderSatisfies : Bool := {
   descr := "Automatically render an application `p e` as `e |=tla= p` when `p` is a TLA formula. "
 }
 
-register_simp_attr tla_nontemporal_def  -- marking the non-temporal parts of TLA
+/-- Marking the non-temporal parts of TLA. -/
+register_simp_attr tla_nontemporal_def
+
+/-- Marking the TLA definitions. -/
 register_simp_attr tlasimp_def
+
+/-- Marking the theorems that can be simplify reasoning at the TLA level. -/
 register_simp_attr tlasimp
-register_simp_attr tladual      -- experimental
+
+/-- Marking the theorems that are dual to some existing theorems. -/
+register_simp_attr tladual
 
 initialize registerTraceClass `lentil.debug
